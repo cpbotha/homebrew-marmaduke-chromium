9e898aea579491ce43c6c76624457daf31d3583b
Shein Htike <chocolateshein.htike@gmail.com>
2019-01-24 18:33:07 -0500

    changed comment

--- Casks/marmaduke-chromium-beta.rb
+++ Casks/marmaduke-chromium-beta.rb
@@ -2,7 +2,7 @@ cask 'marmaduke-chromium-beta' do
 	version '73.0.3637.0'
   sha256 '2f433a2cbd420715c7aee110300e508b1df6c5ad01239cdd746c8dcd8d65cbd5'
 
-  # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
+  # https://github.com/macchrome/chromium/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/chromium/releases/download/v#{version}-r615361-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/chromium/releases.atom'
   name 'Chromium'
