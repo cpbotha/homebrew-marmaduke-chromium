ba69b7f25fbd36620327cb035bda759d8bcba5b2
Shein Htike <chocolateshein.htike@gmail.com>
2019-01-24 18:10:24 -0500

    changed shasum

--- Casks/marmaduke-chromium-beta.rb
+++ Casks/marmaduke-chromium-beta.rb
@@ -1,6 +1,6 @@
 cask 'marmaduke-chromium-beta' do
 	version '73.0.3637.0'
-  sha256 '4982D5A396675835838FCF28553765EA4BCEB16C'
+  sha256 '2f433a2cbd420715c7aee110300e508b1df6c5ad01239cdd746c8dcd8d65cbd5'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/chromium/releases/download/v#{version}-r615361-macOS/Chromium.#{version}.sync.app.zip"
