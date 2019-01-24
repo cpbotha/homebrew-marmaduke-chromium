060aa9222aeafe20a8b958cacca0635c4e05ddc8
Shein Htike <chocolateshein.htike@gmail.com>
2019-01-24 18:24:27 -0500

    added beta nosync with shasum

---
+++ Casks/marmaduke-chromium-beta-nosync.rb
@@ -0,0 +1,12 @@
+cask 'marmaduke-chromium-beta-nosync' do
+	version '73.0.3637.0'
+  sha256 'd1ed8f049b2ae5bc56922f19fe3f0f28337df3ec20cac3f312c6298f72572eb4'
+
+  # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
+  url "https://github.com/macchrome/chromium/releases/download/v#{version}-r615361-macOS/Chromium.#{version}.nosync.app.zip"
+  appcast 'https://github.com/macchrome/chromium/releases.atom'
+  name 'Chromium'
+  homepage 'https://github.com/macchrome/chromium/releases'
+
+  app 'Chromium.app'
+end
