2edfb42f1eff30b06f132fdf572835427b86301f
Shein Htike <chocolateshein.htike@gmail.com>
2019-01-25 18:31:13 -0500

    Parameterized release numbers

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -1,9 +1,15 @@
 cask 'marmaduke-chromium-nosync' do
+  module Utils
+    def self.release
+      599034
+    end
+  end
+
   version '71.0.3578.98'
   sha256 '31477aff9215fc99e53c980120d7242c4b45384f253843bd5be51caa27fe7ba2'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r599034-macOS/Chromium.#{version}.nosync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r#{Utils.release}-macOS/Chromium.#{version}.nosync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,9 +1,14 @@
 cask 'marmaduke-chromium' do
+  module Utils
+    def self.release
+      599034
+    end
+  end
   version '71.0.3578.98'
   sha256 '5fafed5eae92de58ff8a23568f05fd2abd9e43bfde56c9e251cc819a81f77e5e'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r599034-macOS/Chromium.#{version}.sync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r#{Utils.release}-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
