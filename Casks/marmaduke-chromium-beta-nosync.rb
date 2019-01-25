349ad188c338f38b7d87f43cb4e0f5447865a603
Shein Htike <chocolateshein.htike@gmail.com>
2019-01-24 19:06:31 -0500

    release test again

--- Casks/marmaduke-chromium-beta-nosync.rb
+++ Casks/marmaduke-chromium-beta-nosync.rb
@@ -1,12 +1,14 @@
 cask 'marmaduke-chromium-beta-nosync' do
 	module Utils
-		RELEASE = 615361
+		def release
+			615361
+		end
 	end
 	version '73.0.3637.0'
 	sha256 'd1ed8f049b2ae5bc56922f19fe3f0f28337df3ec20cac3f312c6298f72572eb4'
 
 	# https://github.com/macchrome/chromium/releases/ was verified as official when first introduced to the cask
-	url "https://github.com/macchrome/chromium/releases/download/v#{version}-r#{Utils.RELEASE}-macOS/Chromium.#{version}.nosync.app.zip"
+	url "https://github.com/macchrome/chromium/releases/download/v#{version}-r#{Utils.release}-macOS/Chromium.#{version}.nosync.app.zip"
 	appcast 'https://github.com/macchrome/chromium/releases.atom'
 	name 'Chromium'
 	homepage 'https://github.com/macchrome/chromium/releases'
