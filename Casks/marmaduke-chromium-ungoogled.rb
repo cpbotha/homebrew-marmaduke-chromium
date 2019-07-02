da5aecebb6a6cbd60125d680fb8f2367c530fbb8
Andrew67 <andres@andrew67.com>
2019-07-01 22:32:54 -0400

    Update to Chromium 75

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,9 +1,9 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '73.0.3683.86'
-  sha256 'bf82fa22539973e7bb8944375d8a563d4e78aafd58ef3d4486b21d783b713723'
+  version '75.0.3770.90'
+  sha256 '363551b0e04290ed906db8e906187da454781a3cb31ed2d57c18cd5949ad959d'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r652427-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,9 +1,9 @@
 cask 'marmaduke-chromium' do
-  version '73.0.3683.86'
-  sha256 '633d6b05b200a6365f744a0c30191c29cef2102310bc3c7cbce53bac8a5e4786'
+  version '75.0.3770.100'
+  sha256 '7c7f121971b2396c4c7c16b48fe58ebf4f142d64ca431b61c95e8a46de273d51'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-macOS/Chromium.#{version}.sync.app.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r652427-macOS/Chromium.#{version}.sync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
