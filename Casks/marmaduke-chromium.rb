a10e3595ddd819d1c88fec2ef82bcc76e7634bbd
Cadichon <leo.labruyere@pm.me>
2019-09-03 10:31:24 -0400

    Updated sync version to 76.0.3809.100

--- Casks/marmaduke-chromium.rb
+++ Casks/marmaduke-chromium.rb
@@ -1,6 +1,7 @@
 cask 'marmaduke-chromium' do
-  version '73.0.3683.86'
-  sha256 '633d6b05b200a6365f744a0c30191c29cef2102310bc3c7cbce53bac8a5e4786'
+  version '76.0.3809.100'
+  sha1 'bac139f72501d457153c5e82cf953001521dec2d'
+  # sha256 '633d6b05b200a6365f744a0c30191c29cef2102310bc3c7cbce53bac8a5e4786'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-macOS/Chromium.#{version}.sync.app.zip"
