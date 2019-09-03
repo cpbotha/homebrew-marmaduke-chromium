99aba20da5e0532e321b42eab6e620584954a07b
Cadichon <leo.labruyere@pm.me>
2019-09-03 10:37:26 -0400

    Updated ungoogled version to 76.0.3809.100

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,9 +1,8 @@
 cask 'marmaduke-chromium-ungoogled' do
-  version '73.0.3683.86'
-  sha256 'bf82fa22539973e7bb8944375d8a563d4e78aafd58ef3d4486b21d783b713723'
+  version '76.0.3809.100'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
-  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r625896-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
+  url "https://github.com/macchrome/macstable/releases/download/v#{version}-r665002-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
   name 'Chromium'
   homepage 'https://github.com/macchrome/macstable/releases'
