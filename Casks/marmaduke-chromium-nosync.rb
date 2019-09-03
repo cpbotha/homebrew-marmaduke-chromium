32c3c96a6e605737db882a0b8669e731e859dea4
Cadichon <leo.labruyere@pm.me>
2019-09-03 10:41:26 -0400

    Added checksum to nosync

--- Casks/marmaduke-chromium-nosync.rb
+++ Casks/marmaduke-chromium-nosync.rb
@@ -1,6 +1,7 @@
 cask 'marmaduke-chromium-nosync' do
   version '76.0.3809.100'
 
+  sha256 '373e3e633af501e7268296b419f64ba924779cf4f7580d615e8cde05da7d28cc'
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r665002-macOS/Chromium.#{version}.nosync.app.zip"
   appcast 'https://github.com/macchrome/macstable/releases.atom'
