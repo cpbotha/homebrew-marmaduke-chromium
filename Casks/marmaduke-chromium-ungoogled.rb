457b967a0fd2465e071c00f7f1aff71c9e30f612
Cadichon <leo.labruyere@pm.me>
2019-09-03 10:43:21 -0400

    Added checksum to ungoogled

--- Casks/marmaduke-chromium-ungoogled.rb
+++ Casks/marmaduke-chromium-ungoogled.rb
@@ -1,5 +1,6 @@
 cask 'marmaduke-chromium-ungoogled' do
   version '76.0.3809.87'
+  sha256 '8e79627b768543653b672977a422b1aea50105ee5656ebabf43ab7310d4df13a'
 
   # https://github.com/macchrome/macstable/releases/ was verified as official when first introduced to the cask
   url "https://github.com/macchrome/macstable/releases/download/v#{version}-r665002-Ungoogled-macOS/Chromium.app.ungoogled-#{version}.zip"
